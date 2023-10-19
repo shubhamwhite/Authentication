050f4da89251a8f111ca3f93af2b11333202247a
shubhamwhite <shubham@whitelotuscorporation.com>
2023-10-19 15:26:11 +0530

    minor change

--- constant/general/general_message.js
+++ constant/general/general_message.js
@@ -13,5 +13,5 @@ export default {
     BLOCK : 1,
     UNBLOCK : 0
     
-  }
+  },
 } 
--- controller/vehicleInformation/vehicleInfoUpdate.js
+++ controller/vehicleInformation/vehicleInfoUpdate.js
@@ -4,69 +4,72 @@ import User from '../../models/user.js'
 import vehicleInfo from '../../models/vehicleInfo.js'
 
 // vehicle information update
-const vehicleInfoUpdate = async (req,res) => {
+const vehicleInfoUpdate = async (req, res) => {
   try {
     // Get data from the request body
     const { id, capacity, vehicleName, vehicleNumber, city } = req.body
 
     // Check if the user exists and has the 'driver' role
     const user = await User.findOne({ where: { id, role: 'driver' } })
-    
+
     if (!user) {
       return res
         .status(RESPONSE.HTTP_STATUS_CODES.UNAUTHORIZED)
         .json({ message: RESPONSE.MESSAGES.UNAUTHORIZED_USER })
     }
-   
+
     // check if capacity is number formate or not
     const capacityInNumber = Number(capacity)
 
-    if (isNaN (capacityInNumber)) {
-      return res.status(RESPONSE.HTTP_STATUS_CODES.FORBIDDEN).json({ message: `Capacity in must be number format, ${RESPONSE.MESSAGES.FORBIDDEN}` })
-    } 
+    if (isNaN(capacityInNumber)) {
+      return res
+        .status(RESPONSE.HTTP_STATUS_CODES.FORBIDDEN)
+        .json({
+          message: `Capacity in must be number format, ${RESPONSE.MESSAGES.FORBIDDEN}`,
+        })
+    }
 
     // user id is existed or not
     const existingVehicleInfo = await vehicleInfo.findOne({
-      where: { 
+      where: {
         userId: id,
       },
-    }) 
+    })
 
-    if (!existingVehicleInfo) { 
+    if (!existingVehicleInfo) {
       return res.status(RESPONSE.HTTP_STATUS_CODES.NOT_FOUND).json({
         message: RESPONSE.MESSAGES.NOT_FOUND,
       })
     }
 
-    // check vehicle number is existed or not 
+    // check vehicle number is existed or not
     const vehicleNumberExisted = await vehicleInfo.findAll({
       where: {
         vehicleNumber: vehicleNumber,
         userId: {
-          [Op.notIn] : [id]
+          [Op.notIn]: [id],
         },
       },
     })
-    
+
     console.log(vehicleNumberExisted)
     if (vehicleNumberExisted.length > 0) {
       return res.status(RESPONSE.HTTP_STATUS_CODES.CONFLICT).json({
-        message :`Vehicle Number is ${RESPONSE.MESSAGES.CONFLIT.USER_ALREADY_EXISTED}`
+        message: `Vehicle Number is ${RESPONSE.MESSAGES.CONFLIT.USER_ALREADY_EXISTED}`,
       })
     }
-    
 
     // Update the existing vehicle information
     await existingVehicleInfo.update({
       capacity: capacityInNumber,
       vehicleName,
       vehicleNumber,
-      city
+      city,
     })
-    
+
     res.status(RESPONSE.HTTP_STATUS_CODES.OK).json({
-      message: RESPONSE.MESSAGES.OK, 
-      driverInfo:  existingVehicleInfo
+      message: RESPONSE.MESSAGES.OK,
+      driverInfo: existingVehicleInfo,
     })
   } catch (error) {
     console.error(error)
